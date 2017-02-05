0189a6d01e18e3421fa2190487115a4b188d65a7
Vimmey <vmy.chopra@gmail.com>
2017-02-06 00:00:07 +0530

    PHP first file

---
+++ php/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- <style>
+		#ouput{
+			border: 2px;
+		}
+	</style> -->
+</head>
+<body>
+<div id ="output">
+<?php
+$datevalue = date('d:m:y h:i:s');
+echo $datevalue;
+
+$datevalue = date('d:m:y H:i:s');
+echo $datevalue ;
+
+
+$datevalue = date('d:m:y g:i:s A');
+echo $datevalue;
+
+// $name = "This is a variable ";
+// $age= 12;
+// echo $name;
+// echo '<input type="text">';
+// echo '<script>alert(Welcome Php")</script>';
+
+?>
+<!-- <h2> I am heading </h2> -->
+</body>
+</html>
+
