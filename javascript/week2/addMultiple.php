c51a86057632ac04c97ab4e36b6f9e23e3b1163a
Vimmey <vmy.chopra@gmail.com>
2017-02-26 23:34:42 +0530

    funcions

---
+++ javascript/week2/addMultiple.php
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+function sum(){
+	$parameterArray = func_get_args();
+	$sum = 0;
+	for($i=0; $i<count($parameterArray); $i++) {
+		$sum = $sum + $parameterArray[$i];	
+
+	}
+	return $avg;
+	
+}
+	
+$returnVal = sum(4,5,6,7).'<br>';
+$returnValOne = sum(4,5,6,7,8);
+echo  $returnVal;
+echo  $returnValOne;
+</script>/
